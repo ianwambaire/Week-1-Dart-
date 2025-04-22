d6b855c088dd790e9f6339f9364818be769e29b3
ianwambaire <ian.wambaire@strathmore.edu>
2025-04-22 12:52:50 +0300

    type conversion

--- DataTypesImplementation.dart
+++ DataTypesImplementation.dart
@@ -7,4 +7,17 @@ void main() {
   bool isStudent = true;
   print(
       "$name is $age years old and weighs $weight and is a student: $isStudent");
+  //converting a String to an Int and Double
+  String value = "12";
+  int newValue = int.parse(value);
+  print(value.runtimeType);
+  print(newValue.runtimeType);
+  double newdoublevalue = double.parse(value);
+  print(newdoublevalue);
+  //Converting Int to String and Double
+  int oldnumber = 20;
+  String number = oldnumber.toString();
+  print(number.runtimeType);
+  double newNumber = oldnumber.toDouble();
+  print(newNumber);
 }
