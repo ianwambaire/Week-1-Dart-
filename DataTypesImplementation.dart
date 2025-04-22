5e8bf7e7379849b29f235de8db85dc552c17f2a5
ianwambaire <ian.wambaire@strathmore.edu>
2025-04-22 12:42:20 +0300

    Data type implementation

---
+++ DataTypesImplementation.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+
+void main() {
+  String name = "Ian";
+  int age = 20;
+  double weight = 65.0;
+  bool isStudent = true;
+  print(
+      "$name is $age years old and weighs $weight and is a student: $isStudent");
+}
